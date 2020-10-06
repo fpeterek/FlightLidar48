cc2203927b4fe284feebee01a81741bba075cecf
Filip Peterek <fpeterek@seznam.cz>
2020-10-06 19:04:37 +0200

    Rest api with url parameters

--- src/main/java/org/fpeterek/flightlidar48/WebApp.java
+++ src/main/java/org/fpeterek/flightlidar48/WebApp.java
@@ -2,14 +2,24 @@ package org.fpeterek.flightlidar48;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebApp {
 
-  @RequestMapping(value="/", method= RequestMethod.GET)
+  @RequestMapping(value="/", method=RequestMethod.GET)
   public String index() {
     return "html/webapp";
+  }
+
+  @RequestMapping(value="/flights", method=RequestMethod.GET)
+  public String flights(
+    @RequestParam(value="leftTop") String leftTop,
+    @RequestParam(value="rightBottom") String rightBottom
+  ) {
+    System.out.println("latlon: {" + leftTop + ", " + rightBottom + "}");
+    return "{\"flights\": []}";
   }
 
 }
