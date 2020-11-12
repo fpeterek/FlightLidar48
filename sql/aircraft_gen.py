e8aa499fb725ba358972097f1360a6b1b93c142c
Filip Peterek <fpeterek@seznam.cz>
2020-11-12 19:41:27 +0100

    Unfinished aircraft generator

---
+++ sql/aircraft_gen.py
@@ -0,0 +1,153 @@
+#!/usr/bin/python3
+
+import random
+from datetime import date
+
+
+msn_cache = {}
+
+
+type_age = {
+    'A35K': 2,
+    'A359': 3,
+    'A388': 10,
+    'A320': 4,
+    'A321': 3,
+    'BCS1': 4,
+    'BCS3': 4,
+    'B748': 8,
+    'B738': 20,
+    'B77W': 16,
+    'B788': 9,
+}
+
+
+type_manufacturer = {
+    'A35K': 'airbus',
+    'A359': 'airbus',
+    'A388': 'airbus',
+    'A320': 'airbus',
+    'A321': 'airbus',
+    'BCS1': 'airbus',
+    'BCS3': 'airbus',
+    'B748': 'boeing',
+    'B738': 'boeing',
+    'B77W': 'boeing',
+    'B788': 'boeing',
+}
+
+
+airline_fleets = {
+    'LH': ['A359', 'A388', 'A320', 'A321', 'B748'],
+    'LX': ['A320', 'BCS1', 'BCS3', 'B77W'],
+    'EK': ['B77W', 'A388'],
+    'DL': ['A35K', 'A320', 'A321', 'BCS3'],
+    'AA': ['B77W', 'B788', 'B738', 'A320'],
+    'AF': ['A359', 'A388', 'A35K', 'A320', 'A321', 'B77W'],
+    'AC': ['B737', 'B787'],
+    'BA': ['A35K', 'A388', 'A320', 'A321', 'B77W'],
+    'OK': ['A320', 'BCS3', 'B738'],
+    'QS': ['B738'],
+    'SQ': ['A35K', 'A359', 'A388', 'B77W', 'B788']
+}
+
+
+airline_countries = {
+    'LH': 'D',
+    'LX': 'HB',
+    'EK': 'A6',
+    'DL': 'N',
+    'AA': 'N',
+    'AF': 'F',
+    'AC': 'C',
+    'BA': 'G',
+    'OK': 'OK',
+    'QS': 'OK',
+    'SQ': '9V'
+}
+
+
+def get_msn(manufacturer: str) -> int:
+    val = msn_cache.get(manufacturer, 0) + 1
+    msn_cache[manufacturer] = val
+    return val
+
+
+def rand_letter() -> str:
+    return random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+
+
+def gen_registration(prefix: str) -> str:
+    return f'{prefix}-{"".join([rand_letter() for _ in range(5-len(prefix))])}'
+
+
+def rand_type() -> str:
+    return random.choice(type_age)
+
+
+def get_first_flight(max_age: int) -> str:
+    today = date.today()
+
+    min_year = today.year - max_age
+    year = random.choice(range(min_year, today.year+1))
+
+    month = random.randint(0, 12 if year < today.year else today.month) + 1
+
+    # Dates don't really matter other than to ensure age calculation work properly 
+    # so we can ignore the last couple days of the month to make the generator less complicated
+    day_threshold = today.day if year == today.year and month == today.month else 28  
+    day = random.randint(0, day_threshold) + 1
+
+    return date(year, month, day)
+
+
+class Aircraft:
+    def __init__(self, reg: str='', msn: int=0, airline: str='', type_des: str='', first_flight=None):
+        self.reg = reg
+        self.msn = msn
+        self.airline = airline
+        self.type_des = type_des
+        self.first_flight = first_flight
+
+    @staticmethod
+    def random(airline: str):
+        reg = gen_registration(airline_countries[airline])
+        ac_type = rand_type()
+        msn = get_msn(type_manufacturer[ac_type])
+        first_flight = get_first_flight(type_age[ac_type])
+        return Aircraft(reg=reg, msn=msn, airline=airline, type_des=ac_type, first_flight=first_flight)
+
+    @property
+    def formatted_ff(self) -> str:
+        return str(self.first_flight)  # self.first_flight.strftime('%Y-%m-%d')
+
+    @property
+    def insert(self) -> str:
+        return f'INSERT INTO aircraft (registration, msn, airline, type_designator, first_flight) \n    VALUES({self.reg}, {self.msn}, {self.airline} {self.type_des}, {self.first_flight})'
+
+
+def generate(sql, kotlin):
+    aircraft = []
+    for airline in airline_countries:
+        fleet = [Aircraft.random(airline) for _ in range(10)]
+        aircraft += fleet
+
+    kotlin.write('aircraft = listOf(\n')
+
+    for index, ac in enumerate(aircraft):
+        sql.write(ac.insert + '\n')
+        kotlin.write(f'"{ac.reg}"{"," if index != len(aircraft) - 1 else ""}\n')
+
+    kotlin.write(')\n')
+
+
+
+def run():
+    with open('sql.sql', 'w') as sql:
+        with open('kotlin.kt', 'w') as kotlin:
+            generate(sql, kotlin)
+
+
+if __name__ == '__main__':
+    run()
+
--- sql/data.sql
+++ sql/data.sql
@@ -8,12 +8,46 @@ INSERT INTO country (country_name, airport_prefix, registration_prefix) VALUES (
 INSERT INTO country (country_name, airport_prefix, registration_prefix) VALUES ('Singapore', 'WS', '9V');
 INSERT INTO country (country_name, airport_prefix, registration_prefix) VALUES ('United Arab Emirates', 'OM', 'A6');
 
-INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('----', '---', 'None', 17.903, 49.9392, 1);
 INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('KLAX', 'LAX', 'Los Angeles International Airport', -118.408, 33.9425, 1);
 INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('KJFK', 'JFK', 'John F. Kennedy International Airport', -73.7789, 40.64, 1);
 INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('KATL', 'ATL', 'Hartsfield–Jackson Atlanta International Airport', -84.4281, 33.6367, 1);
 INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('CYYZ', 'YYZ', 'Toronto Pearson International Airport', -79.6306, 43.6767, 2);
 INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('EDDF', 'FRA', 'Frankfurt Airport', 8.5706, 50.0333, 3);
 INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('EDDM', 'MUC', 'Munich Airport', 11.7861, 48.3539, 3);
+INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('EGLL', 'LHR', 'Heathrow Airport', -0.4614, 51.4775, 4);
+INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('LFPG', 'CDG', 'Paris Charles de Gaulle Airport', 2.5478, 49.0097, 5);
+INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('LFBO', 'TLS', 'Toulouse–Blagnac Airport', 1.3678, 43.635, 5);
+INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('LSZH', 'ZRH', 'Zurich Airport', 5.5491, 47.4647, 6);
+INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('LKPR', 'PRG', 'Vaclav Havel Airport Prague', 14.26, 50.1008, 7);
+INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('LKMT', 'OSR', 'Leos Janacek Airport Ostrava', 18.1108, 49.6961, 7);
+INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('LKXB', 'KXB', 'Koberice International Airport', 18.038, 49.9571, 7);
+INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('WSSS', 'SIN', 'Singapore Changi Airport', 103.9894, 1.3592, 8);
+INSERT INTO airport (icao, iata, airport_name, lat, lon, country_id) VALUES('OMDB', 'DXB', 'Dubai International Airport', 55.3644, 25.2528, 9);
 
-INSERT INTO flight (number, departure, arrival, origin, destination, aircraft, current_flight) VALUES ('DUMMY', current_timestamp, current_timestamp, '----', '----', '------', 1);
+INSERT INTO aircraft_type (designator, manufacturer, family, subtype) VALUES ('A35K', 'Airbus', 'A350', '-1000');
+INSERT INTO aircraft_type (designator, manufacturer, family, subtype) VALUES ('A359', 'Airbus', 'A350', '-900');
+INSERT INTO aircraft_type (designator, manufacturer, family, subtype) VALUES ('A388', 'Airbus', 'A380', '-800');
+INSERT INTO aircraft_type (designator, manufacturer, family, subtype) VALUES ('A320', 'Airbus', 'A320', 'neo');
+INSERT INTO aircraft_type (designator, manufacturer, family, subtype) VALUES ('A321', 'Airbus', 'A321', 'neo');
+INSERT INTO aircraft_type (designator, manufacturer, family, subtype) VALUES ('BCS1', 'Airbus', 'A220', '-100');
+INSERT INTO aircraft_type (designator, manufacturer, family, subtype) VALUES ('BCS3', 'Airbus', 'A220', '-300');
+INSERT INTO aircraft_type (designator, manufacturer, family, subtype) VALUES ('B748', 'Boeing', '747', '-8i');
+INSERT INTO aircraft_type (designator, manufacturer, family, subtype) VALUES ('B738', 'Boeing', '737', '-800');
+INSERT INTO aircraft_type (designator, manufacturer, family, subtype) VALUES ('B77W', 'Boeing', '777', '-300ER');
+INSERT INTO aircraft_type (designator, manufacturer, family, subtype) VALUES ('B787', 'Boeing', '787', '-800');
+
+INSERT INTO airline (designator, airline_name) VALUES ('LH', 'Lufthansa');
+INSERT INTO airline (designator, airline_name) VALUES ('LX', 'Swiss International Air Lines');
+INSERT INTO airline (designator, airline_name) VALUES ('EK', 'Emirates');
+INSERT INTO airline (designator, airline_name) VALUES ('DL', 'Delta Air Lines');
+INSERT INTO airline (designator, airline_name) VALUES ('AA', 'American Airlines');
+INSERT INTO airline (designator, airline_name) VALUES ('AF', 'Air France');
+INSERT INTO airline (designator, airline_name) VALUES ('AC', 'Air Canada');
+INSERT INTO airline (designator, airline_name) VALUES ('BA', 'British Airways');
+INSERT INTO airline (designator, airline_name) VALUES ('OK', 'CSA');
+INSERT INTO airline (designator, airline_name) VALUES ('QS', 'Smartwings');
+INSERT INTO airline (designator, airline_name) VALUES ('SQ', 'Singapore Airlines');
+
+INSERT INTO aircraft (registration, msn, airline, type_designator, first_flight)
+VALUES ();
+
